bae6ca4131c9bb0f6f0b079b5773458a87e48307
Alex Viscreanu <alexviscreanu@gmail.com>
2018-01-30 23:35:45 +0100

    Import cleanup

--- app/src/main/java/com/dravite/homeux/views/AppIconView.java
+++ app/src/main/java/com/dravite/homeux/views/AppIconView.java
@@ -4,12 +4,9 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Rect;
-import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.util.TypedValue;
-import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.ViewConfiguration;
 import android.widget.Button;
@@ -17,7 +14,6 @@ import android.widget.Button;
 import com.dravite.homeux.views.helpers.CheckForLongPressHelper;
 import com.dravite.homeux.Const;
 import com.dravite.homeux.LauncherUtils;
-import com.dravite.homeux.R;
 import com.dravite.homeux.views.viewcomponents.TextDrawable;
 
 /**
